26cf5267ae3d80872421a8df5cf646f187128fa0
batxes <batxes@gmail.com>
2016-09-30 16:51:58 +0200

    Painting atac seq without outliers

--- src/GenomePainting.py
+++ src/GenomePainting.py
@@ -4,6 +4,8 @@ import sys
 import re
 import ConfigParser
 import pysam
+
+import numpy as np
 from colour import Color
 import heapq
 import matplotlib.cm  as cm
@@ -34,19 +36,13 @@ try:
     viewpoints = viewpoints.split(",")
     viewpoints = [ int(i) for i in viewpoints]
     viewpoints = [int(i/WINDOW) for i in viewpoints]
-    print viewpoints
     
     NFRAGMENTS_ALL = int(config.get("ModelingValues", "NFRAGMENTS"))
     NFRAGMENTS = int(NFRAGMENTS_ALL/WINDOW)
     
-    number_of_models = int(config.get("ModelingValues", "number_of_models"))
     working_dir = config.get("ModelingValues", "working_dir")
 
 
-    subset = int(config.get("AnalysisValues", "subset"))
-    std_dev = int(config.get("AnalysisValues", "std_dev"))
-    jump = number_of_models
-    cut_off_percentage = int(config.get("AnalysisValues", "cut_off_percentage"))
 
     painting_path = config.get("Painting","file_path")
     color_from = config.get("Painting","color_from")
@@ -67,7 +63,7 @@ ends = []
 if bam_or_bed == "bam":
     print "Reading Atac bam file..."
     bamhandle = pysam.AlignmentFile(painting_path,"rb")
-    with open ("bed_file","w") as stdout:
+    with open ("bedbam_file","w") as stdout:
         with open (files[0],"r") as stdin:
             for line in stdin:
                 values = line.split("\t")
@@ -89,7 +85,7 @@ elif bam_or_bed == "bed":
     total_reads = 0
     with open (painting_path,"r") as stdin:
         print "Writing bed file with DNA met data..."
-        with open ("bed_file","w") as stdout:
+        with open ("bedbam_file","w") as stdout:
             for line in stdin:
                 if counter == NFRAGMENTS_ALL:
                     break
@@ -113,7 +109,7 @@ elif bam_or_bed == "bed":
 print "Painting genome..."
 #file format: chr    from    to  value
 bead_values = []
-with open ("bed_file","r") as stdin:
+with open ("bedbam_file","r") as stdin:
     counter = 0
     added_reads = 0
     added_region = 0
@@ -125,21 +121,43 @@ with open ("bed_file","r") as stdin:
         if counter == WINDOW:
             normalized_read = added_reads/added_region
             bead_values.append(normalized_read)
-            print normalized_read
+            #print normalized_read
             counter = 0
             added_region = 0
             added_reads = 0
-    if counter != WINDOW: #we add the min value to the last bead if it did not reach to Nfragments 
+    if counter != WINDOW and counter != 0:  #we add the min value to the last bead if it did not reach to Nfragments 
         normalized_read = added_reads/added_region
         bead_values.append(normalized_read)
 
 from pylab import *
 import matplotlib as mpl
 import matplotlib.cm as cm
-cmap = cm.hot_r
-#cmap = cm.Greens
+#cmap = cm.hot_r
+cmap = cm.Reds
 
-norm = mpl.colors.Normalize(vmin=min(bead_values), vmax=max(bead_values))
+#take out outliers (Q1-1.5xIQR - Q3+1.5*IQR take only)
+Q1 = np.percentile(bead_values,25)
+Q3 = np.percentile(bead_values,75)
+IQR =  Q3 - Q1
+inlier1 = Q1 - 1.5*IQR
+inlier2 = Q3 + 1.5*IQR
+vmax = 0
+for max_value in bead_values:
+    if max_value >= inlier1 and max_value <= inlier2:
+        if max_value > vmax:
+            vmax = max_value
+vmin = 1000000
+for min_value in bead_values:
+    if min_value >= inlier1 and min_value <= inlier2:
+        if min_value <= vmin:
+            vmin = min_value
+print vmin
+print vmax
+print min(bead_values)
+print max(bead_values)
+
+norm = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
+#norm = mpl.colors.Normalize(vmin=min(bead_values), vmax=max(bead_values))
 #norm = mpl.colors.Normalize(vmin=0.01, vmax=0.04) #methylome
 #norm = mpl.colors.Normalize(vmin=0.1, vmax=0.6) #atac-seq
 m = cm.ScalarMappable(norm=norm, cmap=cmap)
@@ -154,7 +172,119 @@ with open("coloring.cmd","w") as colored_model:
 
 print "Now, open coloring.cmd wich Chimera."
 
+dendro_test = False
+if dendro_test:
+    ####################
+    import sys
+    import operator
+    import re
+    import subprocess
+    from os import listdir, remove
+    from os.path import isfile, join
+    from itertools import combinations
+    import ConfigParser
+    import scipy.cluster.hierarchy as sch
+    import pylab
+    from pylab import plot,show
+    from scipy.cluster.vq import kmeans,vq
 
+
+
+    matrix_path = "/home/ibai/4c2vhic/data/Six_zebra_models/Six_zebra_models_final_output_0.1_-0.1_13000/distances_of_current_model_Six_zebra_models"
+    article = True
+    enriched_dendro = True
+    cutoff = 0.25
+    cutoff = 0.0 #ALL beads
+    value_list = []
+    count = 0
+    bead_values = bead_values[0:-1]
+    bead_colors = []
+    if article:
+        for bead in bead_values:
+            bead_colors.append(bead)
+            if enriched_dendro:
+                if bead >= cutoff:
+                    value_list.append(count)
+            else:
+                if bead < cutoff:
+                    value_list.append(count)
+            count += 1
+
+    matrix = np.zeros((len(value_list),len(value_list)))
+
+    count1 = -1
+    for bead1 in value_list:
+        count1 += 1
+        count2 = -1
+        for bead2 in value_list:
+            count2 += 1
+            with open("{}".format(matrix_path), "r") as mtx:
+                for line in mtx:
+                    values = re.split(",", line)
+                    if int(values[0]) ==  bead1 and int(values[1]) == bead2:
+                        matrix[count1][count2] = float(values[2])
+                        matrix[count2][count1] = float(values[2])
+                        break
+
+    D = matrix
+
+    # Compute and plot first dendrogram.
+    fig = pylab.figure(figsize=(8,8))
+
+    ax1 = fig.add_axes([0.09,0.1,0.2,0.6])
+    Y = sch.linkage(D, method='average')
+    Z1 = sch.dendrogram(Y, orientation='right')
+
+    ax1.set_xticks([])
+    ax1.set_yticks([])
+
+    # # Compute and plot second dendrogram.
+    ax2 = fig.add_axes([0.3,0.71,0.6,0.2])
+    Y = sch.linkage(D, method='average')
+    Z2 = sch.dendrogram(Y,orientation='top')
+
+    ax2.set_xticks([])
+    ax2.set_yticks([])
+
+    # Plot distance matrix.
+    axmatrix = fig.add_axes([0.3,0.1,0.6,0.6])
+    idx1 = Z1['leaves']
+    idx2 = Z2['leaves']
+    D = D[idx1,:]
+    D = D[:,idx2]
+    im = axmatrix.matshow(D, aspect='auto', origin='lower', cmap=pylab.cm.YlGnBu)
+
+    axmatrix.set_xticks(range(len(value_list)))
+    ordered_values = [value_list[i] for i in Z1['leaves']]
+    axmatrix.set_xticklabels(ordered_values)
+    axmatrix.xaxis.set_label_position('bottom')
+    axmatrix.xaxis.tick_bottom()
+    pylab.xticks(rotation=-90,fontsize=8)
+
+    axmatrix.set_yticks([])
+
+    # Plot colorbar.
+    axcolor = fig.add_axes([0.91,0.1,0.02,0.6])
+    pylab.colorbar(im, cax=axcolor)
+
+    #scatter plot
+    color = [bead_colors[i] for i in ordered_values]
+    aux = []
+    cut = 0.20
+    for t in color:
+        if t > cut:
+            aux.append(max(bead_values))
+        if t <= cut:
+            aux.append(min(bead_values))
+    color = aux
+
+    axmatrix.scatter(range(len(value_list)), range(len(value_list)), s=80, c=color,vmin=min(bead_values),vmax=max(bead_values),cmap=cm.Reds)
+
+    #fig.show()
+    try:
+            fig.savefig('atac_heatmap.png')
+    except:
+            pass
 
 
 
