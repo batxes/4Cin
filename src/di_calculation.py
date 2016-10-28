d10ad734c5ca3b5e943c1f5c7fd78914879f99a1
batxes <batxes@gmail.com>
2016-10-28 15:34:47 +0100

    di_calculation now gives frecuency of bordes with different TAD sizes. Code needs to be cleaned

--- src/di_calculation.py
+++ src/di_calculation.py
@@ -6,13 +6,17 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+from collections import defaultdict
+import operator
 plt.style.use('ggplot')
 
+tad_from = 10
+tad_to = 50
 
 add_mean_values = True
 
 if len(sys.argv) < 3:
-    print "wrong parameters. Distance Matrix and Tad size (in bins) is required."
+    print "wrong parameters. Distance Matrix and Tad size (in bins) for the plot is required."
     sys.exit()
 input_path = sys.argv[1]
 tad_size = int(sys.argv[2])
@@ -105,14 +109,24 @@ for i in range(size):
 
 #we apply log2 so we have a smaller plotting
 di_list2 = []
+positive = True
+boundary = False
 print di_list
 for i in di_list:
     if i == 0:
         di_list2.append(1)
     if i < 0:
         di_list2.append(math.log(math.fabs(i),2)*-1)
+        if positive:
+            boundary = True
+            positive = False
     if i > 0:
         di_list2.append(math.log(i,2))
+        if positive == False:
+            positive = True
+    if boundary:
+        print "Boundary: {}".format(di_list.index(i))
+        boundary = False
 print di_list2
 index = np.arange(size)
 di_list_pos = []
@@ -131,9 +145,15 @@ for x in di_list:
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.set_xlim(0,size)
+ax.set_ylim(-100,100)
 ax.set_axis_bgcolor('white')
-plt.bar(index,di_list_pos,facecolor='#9999ff')
-plt.bar(index,di_list_neg,facecolor='#ff9999')
+#for mouse
+di_list_neg = [i * -1 for i in di_list_neg] 
+di_list_pos = [i * -1 for i in di_list_pos] 
+plt.bar(index,di_list_neg,facecolor='#9999ff')
+plt.bar(index,di_list_pos,facecolor='#ff9999')
+#plt.bar(index,di_list_pos,facecolor='#9999ff')
+#plt.bar(index,di_list_neg,facecolor='#ff9999')
 
 try:
     saved_in = input_path.split("/")
@@ -144,3 +164,78 @@ except:
     pass
 
 plt.show()
+
+
+### Many times to find the boundaries:
+boundaries = defaultdict(int)
+for tad_size in range(tad_from,tad_to):
+    print tad_size
+
+    di_list = []
+    for i in range(size):
+        #print "bin {}".format(i)
+        upstream = 0
+        up_cont = 0
+        if ((size-i) + tad_size) > size:
+            from_ = 0
+        else:
+            from_ = i - tad_size
+        to_ = i
+        for j in range(from_ ,to_):
+            if j != i:
+                up_cont = up_cont + 1
+                upstream = upstream + matrix[j][i]
+        if add_mean_values: 
+            if up_cont < tad_size:
+                upstream = upstream + mean_value * (tad_size - up_cont)
+        downstream = 0
+        down_cont = 0
+        if i + tad_size < size:
+            to_ = i+tad_size+1 
+        else:
+            to_ = size
+        from_ = i
+        for j in range(from_, to_):
+            if j != i:
+                down_cont = down_cont + 1
+                downstream = downstream + matrix[j][i]
+        if add_mean_values: 
+            if down_cont < tad_size:
+                downstream = downstream + mean_value * (tad_size - down_cont)
+        #apply the equation
+        E = (downstream + upstream)/2
+        di1 = ((downstream-upstream)/(abs(downstream-upstream)))
+        di2 = ((((upstream-E)**2)/E)+((((downstream-E)**2)/E)))
+        di = di1 * di2
+        
+    #print "total= u - d    {}={} - {}".format(total, upstream, downstream)
+        di_list.append(di)
+
+    #for i in range(len(di_list)):
+    #    print "{}: {}".format(i,di_list[i])
+
+
+    ###############
+    #we apply log2 so we have a smaller plotting
+    di_list2 = []
+    positive = True
+    boundary = False
+    for i in di_list:
+        if i == 0:
+            di_list2.append(1)
+        if i < 0:
+            di_list2.append(math.log(math.fabs(i),2)*-1)
+            if positive:
+                boundary = True
+                positive = False
+        if i > 0:
+            di_list2.append(math.log(i,2))
+            if positive == False:
+                positive = True
+        if boundary:
+            #print "Boundary: {}".format(di_list.index(i))
+            boundaries[di_list.index(i)] += 1
+            boundary = False
+sorted_x = sorted(boundaries.items(), key=operator.itemgetter(1), reverse=True)
+for i in sorted_x:
+    print "Boundary:{} - times:{}".format(i[0],i[1])
