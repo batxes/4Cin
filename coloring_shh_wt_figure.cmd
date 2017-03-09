44ad8073a3753934cfd4238609649ec782a584da
batxes <batxes@gmail.com>
2017-03-09 18:33:49 +0100

    Added fig2 command lines

---
+++ coloring_shh_wt_figure.cmd
@@ -0,0 +1,94 @@
+open SHH_WT_models/SHH_WT_models_final_output_0.1_-0.1_11000/Representative.py
+color #b3b3b3 #0  
+color #b3b3b3 #1
+color #b3b3b3 #2
+color #b3b3b3 #3
+color #b3b3b3 #4
+color #b3b3b3 #5
+color #b3b3b3 #6
+color #b3b3b3 #7
+color #b3b3b3 #8
+color #b3b3b3 #9
+color #b3b3b3 #10
+color #b3b3b3 #11
+color #b3b3b3 #12
+color #b3b3b3 #13
+color #b3b3b3 #14
+color #b3b3b3 #15
+color #b3b3b3 #16
+color #b3b3b3 #17
+color #b3b3b3 #18
+color #b3b3b3 #19
+color #b3b3b3 #20
+color #b3b3b3 #21
+color #b3b3b3 #22
+color #b3b3b3 #23
+color #b3b3b3 #24
+color #b3b3b3 #25
+color #b3b3b3 #26
+color #b3b3b3 #27
+color #b3b3b3 #28
+color #b3b3b3 #29
+color #b3b3b3 #30
+color #b3b3b3 #31
+color #b3b3b3 #32
+color #b3b3b3 #33
+color #b3b3b3 #34
+
+color #0055d4 #63
+color #0055d4 #62
+color #0055d4 #61
+color #0055d4 #60
+color #0055d4 #59
+color #0055d4 #58
+color #0055d4 #57
+color #0055d4 #56
+color #558de2 #55
+color #558de2 #54
+color #558de2 #53
+color #558de2 #52
+color #AAc6F0 #51
+color #AAc6F0 #50
+color #ffffff #49
+color #f0e2aa #48
+color #f0e2aa #47
+color #e2c655 #46
+color #e2c655 #45
+color #e2c655 #44
+color #e2c655 #43
+color #d4aa00 #42
+color #d4aa00 #41
+color #d4aa00 #40
+color #d4aa00 #39
+color #d4aa00 #38
+color #d4aa00 #37
+color #d4aa00 #36
+color #d4aa00 #35
+
+
+color #b3b3b3 #64
+color #b3b3b3 #65
+color #b3b3b3 #66
+color #b3b3b3 #67
+color #b3b3b3 #68
+color #b3b3b3 #69
+color #b3b3b3 #70
+shape tube #0-70 radius 200 bandlength 10000
+
+
+
+shape sphere radius 300 center #7 color #819602
+shape sphere radius 300 center #9 color #8f12dd
+shape sphere radius 300 center #54 color #f08500
+shape sphere radius 300 center #58 color #f5143d
+shape cone radius 500 height 600 center #6 color #000000 caps false
+shape cone radius 500 height 600 center #8 color #000000 caps false
+shape cone radius 500 height 600 center #57 color #000000 caps false
+shape cone radius 500 height 600 center #53 color #000000 caps false
+~show #0-100 
+
+background solid white
+
+#verdekaka: 819602
+#naranja: f08500
+
