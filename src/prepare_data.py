44e44dca2103b0122f90dcbec674150bee727f13
batxes <batxes@gmail.com>
2017-06-13 12:07:09 +0200

    little fix in a print

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -23,7 +23,7 @@ def fileCheck(f):
 #MAIN
 ####
 if len(sys.argv) < 2:
-    print "Usage: prepare_data.py config_file 4c-seq_files. Files need to be in this format: \nchromosome start_fragment end_fragment score"
+    print "Usage: prepare_data.py 4c-seq_files. Files need to be in this format: \nchromosome start_fragment end_fragment score"
     sys.exit()
 frag_dict = {}
 for i in sys.argv[1:]:
